bdfebfa5f53cb0da31f1c61f0677447187388a42
Nelly-Margaryan <nellymargaryaan@gmail.com>
2023-03-25 00:33:45 +0400

    Lesson 64-65 Delete Task, Delete Selected

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from 'react-bootstrap';
 import './App.css';
-import ToDo from './ToDo-List';
-import {Task} from "./ToDo-List";
+import ToDo from './Todo-components/Todo/ToDo-List';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <ToDo />
-        <Task />
-
-
-
-
-      </header>
-      
-    </div>
+    <main>
+      <ToDo />
+    </main>
   );
 }
 
---
+++ src/utils/helpers.js
@@ -0,0 +1,4 @@
+
+export function idGenerator (){
+    return Math.random().toString(36) + Math.random().toString(36);
+}
