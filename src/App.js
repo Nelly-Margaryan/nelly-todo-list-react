3894c840404b6762be754102f4e34d9a0f9954b8
Nelly-Margaryan <nellymargaryaan@gmail.com>
2023-04-28 19:45:42 +0400

    The folders were changed to capitals

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-datepicker/dist/react-datepicker.css";
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
-import ToDo from './components/todo/ToDo-List';
+import ToDo from './Components/Todo/ToDo-List';
 
 function App() {
   return (
---
+++ src/Utils/helpers.js
@@ -0,0 +1,10 @@
+
+export function formatDate (date){
+    if(date instanceof Date){
+        return date.toISOString().slice(0, 10); 
+    }
+    if(typeof date === "string"){
+        return date.slice(0, 10);
+    }
+    return "";
+}
